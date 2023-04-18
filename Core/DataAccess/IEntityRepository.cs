2ffe1bd2e5584ecadf857dc91b1204583f41da04
alihsynv <ali.hsynov@gmail.com>
2023-04-18 12:23:19 +0400

    🛠️📝 Cade Refactoring

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstarct
+namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T : class, new()
     {
--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Abstarct
+namespace Core.Entities
 {
     public interface IEntity
     {
